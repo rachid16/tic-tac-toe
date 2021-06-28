7d14f9901dbd0d5e774f909c8e61526312e2d7a3
Rachid <azeroual.rachid1@gmail.com>
2021-06-28 15:51:30 +0100

    update

--- src/com/pyramid/Main.java
+++ src/com/pyramid/Main.java
@@ -1,30 +1,50 @@
 package com.pyramid;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Integer> toUse = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
     static String[] table = new String[9];
     public static void main(String[] args) {
+        String winner ="";
         String player = "X";
         System.out.println("Welcome to Tic-Tac-Toe!");
-        System.out.println("Do you want to be X or O?");
-        player = input.nextLine().toUpperCase();
-        System.out.println("The computer will go first.");
-        System.out.println("What is your next move? (1-9)");
-        int nextMove = Integer.parseInt(input.nextLine());
-        for (int i= 0; i<9; i++)
-            table[i]=" ";
+        toUse.addAll(table);
+        do {
+            System.out.println("Do you want to be X or O?");
+            player = input.nextLine().toUpperCase();
+            System.out.println("The computer will go first.");
 
-        if ((table[nextMove-1].equals("X")) || (table[nextMove-1].equals("O"))) {
-            System.out.println("this number already token");
-        }
-        else {
-            table[nextMove - 1] = player;
-        }
-        printBoard();
+            for (int i= 0; i<9; i++)
+                table[i]=" ";
 
+            while(winner.equals("")){
+
+                System.out.println("What is your next move? (1-9)");
+                int nextMove = Integer.parseInt(input.nextLine());
+
+                if ((table[nextMove-1].equals("X")) || (table[nextMove-1].equals("O"))) {
+                    System.out.println("this number already token");
+                }
+                else {
+                    table[nextMove - 1] = player;
+                }
+                printBoard();
+                if(findWinner("XXX"))
+                    winner = "X";
+                else if(findWinner("OOO"))
+                    winner = "O";
+                else
+                    winner ="";
+            }
+            if(winner.equals("X") && player.equals("X"))
+                System.out.println("you have beaten the computer! You won.");
+            else
+                System.out.println("The computer has beaten you! You lose.");
+        }while (promptTryAgain());
 
 
 
@@ -71,15 +91,19 @@ public class Main {
     }
     static boolean promptTryAgain () {
         while (true) {
-            System.out.print("Would you like to start a new game? (Y/N): ");
+            System.out.print("Do you want to play again? (yes or no)");
             String response = input.next();
-            if (response.equalsIgnoreCase("y")) {
+            if (response.equalsIgnoreCase("yes")) {
                 return true;
-            } else if (response.equalsIgnoreCase("n")) {
+            } else if (response.equalsIgnoreCase("no")) {
                 return false;
             }
             System.out.println("Invalid input.");
         }
+    }
+
+    static void computerTurn(){
+
     }
 }
 
